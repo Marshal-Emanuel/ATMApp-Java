b074a90b84a47a4fddeb26f5e6c6bb047459167a
Emanuel Marshal <97548460+Marshal-Emanuel@users.noreply.github.com>
2023-11-08 01:17:39 +0300

    Update LoginScreen.java

--- ATM-Java/src/LoginScreen.java
+++ ATM-Java/src/LoginScreen.java
@@ -14,7 +14,7 @@ public class LoginScreen extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        // Cr
+        //
         accountNumberField = new JTextField(20);
         pinField = new JPasswordField(20);
         JButton loginButton = new JButton("Login");
